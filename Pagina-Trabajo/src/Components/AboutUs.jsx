c6b1f07770554de8c301e536660db6500c40cfce
Allan Panchi <avpanchi@espe.edu.ec>
2024-05-18 11:31:39 -0500

    Creación Navbar y de componente About Us

---
+++ Pagina-Trabajo/src/Components/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Styles/AboutUs.css'
+const AboutUs = () => {
+    return (
+        <section className="inicio" id="inicio">
+            <div className="contenedor-banner">
+                <div className="banner" id="banner">
+                    <div className="textos-banner">
+                        <h1>SOBRE NOSOTROS</h1>
+                        <p>Conoce más sobre nosotros</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutUs;
---
+++ Pagina-Trabajo/src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../Styles/NavBar.css'
+
+const Navbar = () => {
+
+    const [menuVisble, setMenuVisible] = useState(false);
+
+
+    const mostrarOcultarMenu = () => {
+        setMenuVisible(!menuVisble);
+    }
+
+    const seleccionar = () => {
+        setMenuVisible(false);
+    }
+
+    return (
+        <section className="contenedor-header">
+        <header>
+            <div className="logo">
+                <a href='#'>Pollito Feliz</a>
+            </div>
+            <nav id="nav" className={menuVisble ? 'responsive':''}>
+                <ul>
+                    <li><Link to='/contacto' onClick={seleccionar}>Inicio</Link></li>
+                    <li><Link to='/sobre-nosotros' onClick={seleccionar}>Sobre Mi</Link></li>
+                    <li><Link to='/' onClick={seleccionar}>Habilidades</Link></li>
+                </ul>
+            </nav>
+            <div className="nav-responsive" onClick={mostrarOcultarMenu}>
+                <FontAwesomeIcon icon="fa-solid fa-bars" />
+            </div>
+        </header>
+    </section>
+    );
+};
+
+export default Navbar;
