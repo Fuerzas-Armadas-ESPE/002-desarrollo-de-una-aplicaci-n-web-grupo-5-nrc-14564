b337507d88175b80802bb7d5bbbe9d2ea01993ad
Allan Panchi <avpanchi@espe.edu.ec>
2024-05-18 20:05:54 -0500

    Cambio de ruta

--- Pagina-Trabajo/src/App.jsx
+++ Pagina-Trabajo/src/App.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutUs from './Components/AboutUs';
 import Navbar from './Components/Navbar'; 
-import Formulario from './components/Formulario';
+import Formulario from './Components/Formulario';
 
 const Home = () => <h2>Home</h2>;
 
 function App() {
-  return (
+    return (
     <Router>
-      <Navbar />
-      <Routes>
+        <Navbar />
+        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contacto" element={<Formulario />} />
         <Route path="/sobre-nosotros" element={<AboutUs />} />
-      </Routes>
+        </Routes>
     </Router>
-  );
+    );
 }
 
 export default App;
