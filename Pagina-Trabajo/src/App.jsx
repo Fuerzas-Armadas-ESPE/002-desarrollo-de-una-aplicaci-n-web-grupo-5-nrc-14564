18abd98622c15bfce831b8e27f1b7304283e2ea0
Alejo Andrade <02alejo20@gmail.com>
2024-05-18 22:38:38 -0500

    Fixing probems

--- Pagina-Trabajo/src/App.jsx
+++ Pagina-Trabajo/src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutUs from './Components/AboutUs';
-<<<<<<< HEAD
 import Formulario from './Components/Formulario';
 import Navbar from './Components/Navbar';
-=======
-import Navbar from './Components/Navbar'; 
-import Formulario from './Components/Formulario';
->>>>>>> b337507d88175b80802bb7d5bbbe9d2ea01993ad
 
 const Home = () => <h2>Home</h2>;
 
